e31434100ed96262ed19979a2776a98cacda042e
Evan Thacker <thackerevan@gmail.com>
2021-05-10 19:00:13 -0400

    update edit code

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
- <?php get_header(); ?>
+<?php get_header(); ?>
     <div class="main">
         <div class="container">
             <?php if(have_posts()) : ?>
-
-            <?php while(have_posts()): the_post(); ?>
-
-                <h3>
+                <?php while(have_posts()): the_post(); ?>
+                    <h3>
                     <a href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
                     </a>
-                    
-                </h3>
-
-                <div class="meta">
-                Created By <?php the_author(); ?> on <?php the_date(); ?>
-                </div>
-
-
-            <?php the_content(); ?>
-
-            <?php endwhile; ?>
-
+                     </h3>
+                    <div class="meta">
+                    Created By <?php the_author(); ?> on <?php the_date(); ?>
+                    </div>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
             <?php else : ?>
-
-            <?php echo wpautop('Sorry, No posts were found.'); ?>
-
-             <?php endif; ?>
-
+                <?php echo wpautop('Sorry, No posts were found.'); ?>
+            <?php endif; ?>
         </div>
     </div>
 
