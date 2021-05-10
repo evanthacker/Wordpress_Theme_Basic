6e371f2b94ca86bfef94bd6cafb77ea3085a3cb3
Evan Thacker <thackerevan@gmail.com>
2021-05-10 07:21:47 -0400

    php & image

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+    
+</body>
+</html>
