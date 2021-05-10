dd0902275ff0e5a79db29d7469853e63519ad043
Evan Thacker <thackerevan@gmail.com>
2021-05-10 12:59:36 -0400

    Loop Lesson

--- index.php
+++ index.php
@@ -1,44 +1,32 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header>
-        <h1><?php bloginfo('name'); ?></h1>
-        <span><?php bloginfo('description'); ?></span>
-    </header>
-
+ <?php get_header(); ?>
     <div class="main">
-    <?php if(have_posts()) : ?>
+        <div class="container">
+            <?php if(have_posts()) : ?>
 
-        <?php while(have_posts()): the_post(); ?>
+            <?php while(have_posts()): the_post(); ?>
 
-            <h3><?php the_title(); ?></h3>
+                <h3><?php the_title(); ?></h3>
 
-            <div class="meta">
-            Created By <?php the_author(); ?> on <?php the_date(); ?>
-            </div>
+                <div class="meta">
+                Created By <?php the_author(); ?> on <?php the_date(); ?>
+                </div>
 
 
             <?php the_content(); ?>
 
-        <?php endwhile; ?>
+            <?php endwhile; ?>
 
-    <?php else : ?>
+            <?php else : ?>
 
-        <?php echo wpautop('Sorry, No posts were found.'); ?>
+            <?php echo wpautop('Sorry, No posts were found.'); ?>
 
-    <?php endif; ?>
+             <?php endif; ?>
 
-    <?php if(){
+            <?php if(){
 
-    }
-    ?>
+             }
+             ?>
+        </div>
     </div>
-    
-</body>
-</html>
+
+<?php get_footer(); ?>
