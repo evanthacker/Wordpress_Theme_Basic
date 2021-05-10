780f62174da89ac142c8544cad4d915beeccd9b5
Evan Thacker <thackerevan@gmail.com>
2021-05-10 07:56:09 -0400

    wordpress loop

--- index.php
+++ index.php
@@ -7,6 +7,38 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <span><?php bloginfo('description'); ?></span>
+    </header>
+
+    <div class="main">
+    <?php if(have_posts()) : ?>
+
+        <?php while(have_posts()): the_post(); ?>
+
+            <h3><?php the_title(); ?></h3>
+
+            <div class="meta">
+            Created By <?php the_author(); ?> on <?php the_date(); ?>
+            </div>
+
+
+            <?php the_content(); ?>
+
+        <?php endwhile; ?>
+
+    <?php else : ?>
+
+        <?php echo wpautop('Sorry, No posts were found.'); ?>
+
+    <?php endif; ?>
+
+    <?php if(){
+
+    }
+    ?>
+    </div>
     
 </body>
 </html>
