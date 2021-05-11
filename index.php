f28c5ec43fb6695399c418cd38d38dd8adab461c
Evan Thacker <thackerevan@gmail.com>
2021-05-10 21:27:34 -0400

    Single php

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
         <div class="container">
             <?php if(have_posts()) : ?>
                 <?php while(have_posts()): the_post(); ?>
-                    <h3>
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                     </h3>
-                    <div class="meta">
-                    Created By <?php the_author(); ?> on <?php the_date(); ?>
-                    </div>
-                    <?php the_content(); ?>
+                    <article class="post">
+                        <h3>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </h3>
+                            <div class="meta">
+                            Created By <?php the_author(); ?> on <?php the_date(); ?>
+                            </div>
+                        <?php the_content(); ?>
+                        <br>
+                        <a class="button" href="<?php the_permalink(); ?>">
+                                Read More
+                            </a>
                 <?php endwhile; ?>
+                    </article>
             <?php else : ?>
                 <?php echo wpautop('Sorry, No posts were found.'); ?>
             <?php endif; ?>
