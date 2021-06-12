de8ee839a1a21d6d9f7c3816ccb812f1aba77b23
Evan Thacker <thackerevan@gmail.com>
2021-06-12 15:26:32 -0400

    Nav Menu & Pages

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 function simple_theme_setup(){
     // Featured Image Support
     add_theme_support('post-thumbnails');
+
+    //Menu
+    register_nav_menu( array(
+        'primary'   => _()
+    ));
 }
 
 add_action('after_setup_theme', 'basic_theme_setup');
